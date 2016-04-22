a07c261e4c0aa7c84c050ac3d7e9d0264c7d6f0b
minhchuduc <chu.ducminh@gmail.com>
2016-04-22 11:11:35 +0700

    Use interface{} to make map() work with any data type

--- Miek/Q12.go
+++ Miek/Q12.go
@@ -4,6 +4,9 @@ import "fmt"
 
 func main() {
 	fmt.Println(mapx(double, []int{1, 2, 3}))
+
+	// Must use []e, can not use []string
+	fmt.Println(mapArbitrary(double2, []e{"asdf", "uop"}))
 }
 
 // TODO: improve it to work on arbitrary function/array
@@ -17,4 +20,26 @@ func mapx(f func(i int) int, ar []int) (ret []int) {
 
 func double(i int) int {
 	return i + i
+}
+
+type e interface{}
+
+func double2(j e) e {
+	switch j.(type) {
+	case int:
+		return j.(int) * 2
+	case string:
+		return j.(string) + j.(string)
+	case float64:
+		return j.(float64) * 2
+	}
+	return j // if don't match "int" / "string" / "float64"
+}
+
+func mapArbitrary(f func(e) e, ar []e) []e {
+	ret := make([]e, len(ar))
+	for i, v := range ar {
+		ret[i] = f(v)
+	}
+	return ret
 }
